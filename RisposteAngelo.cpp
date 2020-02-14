bec7819a0e86d079b870a9771e5aa8dd48a3837d
angelocarb <angcarbwork@gmail.com>
2020-02-14 16:28:26 +0100

    Continuando con risposte ...

--- RisposteAngelo.cpp
+++ RisposteAngelo.cpp
@@ -41,4 +41,20 @@ dalla MMU. Letto quell'indirizzo la routine carica la pagina che lo contiene e t
 le tabelle necessarie per la traduzione. (13 Maggio 2019 - pag6)
 
 /********************************************************************************************/
+/********************************************************************************************/
 
+4) Come avviene la traduzione tra indirizzo virtuale e indirizzo fisico. Dove si
+trovano le tabelle di livello 4 e da chi vengono caricate?Quale compito svolge
+il registro cr3? L'offset da 12 bit quali vincoli comporta nella memorizzazione
+delle pagine nella memoria? Quando è attiva la memoria virtuale a quale parte, il
+software, della memoria fisica può accedere?
+
+-> Al primo passo la MMU deve leggere il corretto descrittore (CR3 punta alla tabella di
+livello 4 del processo in esecuzione, le tabelle di livello 4 si trovano nella parte M2
+della memoria protetta e vengono caricate dal bootstrap). Sia i4 l'indice di livello 4
+(bit 39-47). L'indirizzo a cui vuole leggere è dunque CR3 +i4*8. Letto il descrittore 
+di livello 4, la MMU ne estrae il campo indirizzo (bit 12-51), lo concatena con i bit 30-38
+di V (indirizzo virtuale) e con altri 3 bit a 0, ottendendo cosi l'indirizzo del descrittore
+di livello 3. E cosi via per i descrittori di livello 2 e 1. Arrivata al livello 1, la MMU 
+estrae il campo F (12-51), lo concatena con l'offset di V (bit 0-11) e ottiene cosi la
+traduzione. (6 Maggio 2019 - pag8).
