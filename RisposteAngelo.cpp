5d32b6e9af7d368abc3196083426f82321f50237
angelocarb <angcarbwork@gmail.com>
2020-02-14 17:40:00 +0100

    rispondendo ad altre ... stiamo inguaiati forte

--- RisposteAngelo.cpp
+++ RisposteAngelo.cpp
@@ -1,3 +1,8 @@
+/***************************************************************************/
+Delle 54 domande ho risposto alla 1,4,6.
+
+/*************************************************************************/
+
 1) Quali informazioni ci sono nel descrittore di pagina fisica. A cosa serve
 ricordare l'indirizzo virtuale nel registro del processore cr2, durante il page-fault.
 A cosa serve ricordarmi l'indirizzo del blocco di memoria di massa?
@@ -41,7 +46,6 @@ dalla MMU. Letto quell'indirizzo la routine carica la pagina che lo contiene e t
 le tabelle necessarie per la traduzione. (13 Maggio 2019 - pag6)
 
 /********************************************************************************************/
-/********************************************************************************************/
 
 4) Come avviene la traduzione tra indirizzo virtuale e indirizzo fisico. Dove si
 trovano le tabelle di livello 4 e da chi vengono caricate?Quale compito svolge
@@ -58,3 +62,83 @@ di V (indirizzo virtuale) e con altri 3 bit a 0, ottendendo cosi l'indirizzo del
 di livello 3. E cosi via per i descrittori di livello 2 e 1. Arrivata al livello 1, la MMU 
 estrae il campo F (12-51), lo concatena con l'offset di V (bit 0-11) e ottiene cosi la
 traduzione. (6 Maggio 2019 - pag8).
+-> L'offset dei 12 bit credo comporti che le pagine debbano essere grandi almeno 4KiB.
+
+/*******************************************************************************************/
+6) Spiegare come è fatto un handler. Guardando il codice quanti handler abbiamo,uno solo o molti? Una volta caricato il processo esterno si può fare delle
+predizioni sui punti dove ripartirà? Perchè sono due i punti su cui può ripartire il
+processo esterno? Che compito svolge la EOI in tutto ciò?
+
+-> l'handler ha il solo scopo di mandare in esecuzione un processo, il quale si preoccupa
+di svolgere le operazioni che prima erano svolte dal driver.
+Abbiamo molti handler, non solo uno:
+	handler_i:
+		call salva_stato
+		call inspronti
+		movq $i,%rcx
+		movq a_p(, %rcx,8),%rax
+		movq %rax,esecuzione
+		call carica_stato
+		iretq
+Per ogni possbile interruzione il modulo sistema deve predisporre un handler che si preoccupa
+di mettere in esecuzione il corrispondente processo esterno, prendendo il proc_elem dalla
+tabella a_p.
+
+-> Dopo la loro creazione i processi esterni non terminano più, e dopo aver risposto ad una
+richiesta di interruzione si limitano a sospendersi in attesa di un'altra. Quindi fanno un ciclo infinito e dopo ogni iterazione del for terminano con wfi(). la primitiva wfi() salva lo stato del processo esterno, invia l'EOI all'APIC e mette in esecuzione un altro processo di fatto sospendendo il processo esterno, che potrà riandare in esecuzione solamente quando il corrispondende handler verrà nuovamente invocato, in risposta ad una nuova richiesta di 
+interruzione della stessa interfaccia. SI NOTI che dopo la prima volta che il processo esterno viene avviato, la coppia call carica_stato, iretq nel codice dell'handler caricherà
+lo stato salvato dall wfi(). Infatti se l'handler è in esecuzione vuol dire che l'interfaccia
+ha inviato una nuova richiesta che l'APIC ha fatto passare e dunque esso stesso deve aver
+ricevuto una EOI per la richiesta precendente, e dunque l'ultima cosa che il processo esterno
+aveva fatto in precendenza era proprio chiamare la wfi().
+
+/*******************************************************************************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/******************************************************************************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
